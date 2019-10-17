fc293a9d03727351546ebd440c00e7d83c73219c
spycrab <spycrab@users.noreply.github.com>
2019-10-17 23:58:12 +0200

    Use a consistent quote style

--- qtsdkmin/__main__.py
+++ qtsdkmin/__main__.py
@@ -9,7 +9,7 @@ import requests
 import defusedxml.ElementTree as ET
 import libarchive.public
 
-BASE_REPO_URL = 'http://download.qt.io/online/qtsdkrepository'
+BASE_REPO_URL = "http://download.qt.io/online/qtsdkrepository"
 
 CONFIG = {}
 
@@ -23,7 +23,7 @@ urls={}
 def add_package(package, url):
     global packages, urls
     
-    name = package.find('Name').text
+    name = package.find("Name").text
     
     packages[name] = package
     urls[name] = url
@@ -31,31 +31,31 @@ def add_package(package, url):
 def install_package(package_name):
     global CONFIG, urls
 
-    if package_name in CONFIG['ignore']:
+    if package_name in CONFIG["ignore"]:
         return
         
     if package_name not in packages:
-        print("Package '{}' not found".format(package_name))
+        print("Package "{}" not found".format(package_name))
         exit(1)
 
     package = packages[package_name]
 
-    dependencies = package.find('Dependencies')
+    dependencies = package.find("Dependencies")
     
     if dependencies is not None:
-        for requirement in dependencies.text.split(', '):
+        for requirement in dependencies.text.split(", "):
             install_package(requirement)
 
-    archives = package.find('DownloadableArchives')
+    archives = package.find("DownloadableArchives")
 
     if archives is not None:
         if archives.text is not None:
-            for archive in archives.text.split(', '):
+            for archive in archives.text.split(", "):
 
-                if package_name in CONFIG['config']:
-                    if CONFIG['config'][package_name]['ignore_archives']:
+                if package_name in CONFIG["config"]:
+                    if CONFIG["config"][package_name]["ignore_archives"]:
                         ignored = False
-                        for regex in CONFIG['config'][package_name]['ignore_archives']:
+                        for regex in CONFIG["config"][package_name]["ignore_archives"]:
                             if re.match(regex, archive):
                                 ignored = True
                                 break
@@ -64,7 +64,7 @@ def install_package(package_name):
                             print("Ignoring {}...".format(archive))
                             continue
                 
-                url = urls[package_name]+'/'+package_name+'/'+package.find('Version').text+archive
+                url = urls[package_name]+"/"+package_name+"/"+package.find("Version").text+archive
                 print("Downloading {}...".format(url))
 
                 r = requests.get(url)
@@ -78,32 +78,32 @@ def install_package(package_name):
 def add_repository(url):
     global args
     
-    repo = get_xml(url+'/Updates.xml')
+    repo = get_xml(url+"/Updates.xml")
 
     # Add repositories
-    for repo in repo.iter('Repository'):
+    for repo in repo.iter("Repository"):
 
-        url = repo.attrib['url']
-        action = repo.attrib['action']
-        displayname = repo.attrib['displayname']
+        url = repo.attrib["url"]
+        action = repo.attrib["action"]
+        displayname = repo.attrib["displayname"]
         
-        if action != 'add':
+        if action != "add":
             continue
 
         if args.verbose:
-            print("Adding '{}'...".format(repo.attrib['displayname']))
+            print("Adding "{}"...".format(repo.attrib["displayname"]))
 
         add_repository(url)
 
     # Add packages
-    for package in repo.iter('PackageUpdate'):
+    for package in repo.iter("PackageUpdate"):
         add_package(package, url)
 
 
-parser = argparse.ArgumentParser(description='Create a minimal Qt install')
+parser = argparse.ArgumentParser(description="Create a minimal Qt install")
 
-parser.add_argument('manifest', type=str)
-parser.add_argument('--verbose', action='store_true')
+parser.add_argument("manifest", type=str)
+parser.add_argument("--verbose", action="store_true")
 
 args = parser.parse_args()
 
@@ -111,10 +111,10 @@ stream = open(args.manifest, "r")
 CONFIG = yaml.load(stream)
 
 print("Parsing repositories (this may take a while)...")
-for repo in CONFIG['repos']:
-    add_repository(BASE_REPO_URL + '/' + CONFIG['platform'] + '/' + repo)
+for repo in CONFIG["repos"]:
+    add_repository(BASE_REPO_URL + "/" + CONFIG["platform"] + "/" + repo)
 
 print("Done. {} packages found.".format(len(packages)))
 
-for package in CONFIG['packages']:
+for package in CONFIG["packages"]:
     install_package(package)
